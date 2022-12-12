0f8f86be382288038f96797954a58f22d1983743
JalalHamo <jalal.h2@gso.schule.koeln>
2022-12-12 09:37:27 +0100

    Alle 5 Tests für Auto Klas

---
+++ Fuhrparkverwaltung/Auto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuhrparkverwaltung
+{
+    public class Auto
+    {
+        private double kilometerstand;
+        private double tankinhalt;
+        private double verbrauchProHundertKilometer;
+
+        public Auto (double kilometerstand, double tankinhalt, double verbrauchProSekunde)
+        {
+            this.kilometerstand = kilometerstand;
+            this.tankinhalt = tankinhalt;
+            this.verbrauchProHundertKilometer = verbrauchProSekunde;
+        }
+
+        public Auto(double kilometerstand)
+        {
+            this.kilometerstand = kilometerstand;
+        }
+
+        public void Fahren (double streckeInKilmeter)
+        {
+            if(streckeInKilmeter >= 0)
+            {
+                if(this.tankinhalt >= (verbrauchProHundertKilometer / 100) * streckeInKilmeter)
+                {
+                    this.tankinhalt -= (verbrauchProHundertKilometer / 100) * streckeInKilmeter;
+                    this.kilometerstand += streckeInKilmeter;
+                }
+                else
+                {
+                    this.kilometerstand += (this.tankinhalt * 100) / this.verbrauchProHundertKilometer;
+                    this.tankinhalt = 0;
+                }
+            }
+        }
+
+        public double Kilometerstand
+        {
+            get { return this.kilometerstand; } 
+        }
+
+        public double Tankinhalt
+        {
+            get { return this.tankinhalt; }
+        }
+
+        public double VerbrauchProHundertKilometer
+        {
+            get { return this.verbrauchProHundertKilometer; }
+        }
+    }
+}
--- FuhrparkverwaltungTests/AutoTests.cs
+++ FuhrparkverwaltungTests/AutoTests.cs
@@ -1,24 +1,107 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Fuhrparkverwaltung;
+using Microsoft.VisualBasic;
+using System;
 
 namespace FuhrparkverwaltungTests
 {
     [TestClass]
     public class AutoTests
     {
+        //1
         [TestMethod]
         public void Fahren_SteigertKilometerstand()
         {
             //Arrange
-            int kilometerstand = 0;
+            double kilometerstand = 0;
             Auto a = new Auto(kilometerstand);
-            int streckeInKilometern = 50;
+            double streckeInKilometern = 50;
 
             //Act
             a.Fahren(streckeInKilometern);
 
             //Assert
             Assert.AreEqual(50, a.Kilometerstand);
+        }
+
+        //2
+        [TestMethod]
+        public void Fahren_negativeWerteAendernKilometerstandNicht()
+        {
+            //Arrange
+            double kilometerstand = 10;
+            Auto b = new Auto(kilometerstand);
+            double streckeInKilometern = -1;
+
+            //Act
+            b.Fahren(streckeInKilometern);
+
+            //Assert
+            Assert.AreEqual(kilometerstand, b.Kilometerstand);
+        }
+
+        //3
+        [TestMethod]
+        public void Fahren_sinktTankinhalt()
+        {
+            //Arrange
+            double kilometerstand = 0;
+            double tankinhalt = 10;
+            double verbrauchProHundertKilometer = 5.7;
+            Auto c = new Auto(kilometerstand, tankinhalt, verbrauchProHundertKilometer);
+
+            double streckeInKilometern = 100;
+            double tankinhalt_Soll = 4.3;
+
+
+            //Act
+            c.Fahren(streckeInKilometern);
+
+            //Assert
+            Assert.AreEqual(tankinhalt_Soll, c.Tankinhalt);
+        }
+
+        //4
+        [TestMethod]
+        public void Fahren_LangeZeitMachtTankinhaltNichtMinus()
+        {
+            //Arrange
+            double kilometerstand = 0;
+            double tankinhalt = 10;
+            double verbrauchProHundertKilometer = 5.7;
+            Auto d = new Auto(kilometerstand, tankinhalt, verbrauchProHundertKilometer);
+
+            double streckeInKilometern = 200;
+            double tankinhalt_Soll = 0;
+
+
+            //Act
+            d.Fahren(streckeInKilometern);
+
+            //Assert
+            Assert.AreEqual(tankinhalt_Soll, d.Tankinhalt);
+        }
+
+        //5
+        [TestMethod]
+        public void Fahren_SteigertKilometerstandNurSolangeTankinhaltReicht()
+        {
+            //Arrange
+            double kilometerstand = 0;
+            double tankinhalt = 10;
+            double verbrauchProHundertKilometer = 5.7;
+            Auto e = new Auto(kilometerstand, tankinhalt, verbrauchProHundertKilometer);
+
+            double streckeInKilometern = 200;
+            double kilometerstand_Soll = 175;
+
+
+            //Act
+            e.Fahren(streckeInKilometern);
+            int kilometerstand_Ist = Convert.ToInt32(e.Kilometerstand);
+
+            //Assert
+            Assert.AreEqual(kilometerstand_Soll, kilometerstand_Ist);
         }
     }
 }
